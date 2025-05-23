a030bd9b0c0fc9b40bce4f667012f7766367e935
Arun <hiarunpradhan@gmail.com>
2025-05-23 16:01:00 +0530

    video 9

---
+++ src/models/user.model.js
@@ -0,0 +1,104 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // makes searching easier and optimized
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Middleware to encrypt password
+/*
+userSchema.pre("save",function () {})
+The function will be triggered before saving or updating any user data. Here, 'pre' is a middleware, 'save' is an event (but also a previously defined function in mongoose). 
+We can't use the arrow function here, because we need to have a context for it. So 'function' will be used.
+We need to make it async bcz encryption will take some time.
+Middleware will access to 'next' flag, from other flags (err,req,res,next), in order to forward the request. 
+*/
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // if passowrd is modified, then only run below code and encrypt it.
+  this.password = bcrypt.hash(this.password, 10); // here 10 is the salt to be used in encryption. If specified as a number then a salt will be generated with the specified number of rounds and used.
+  next();
+});
+
+
+// We can create custom methods. '.methods' allows to create custom functions.
+// Custom function to ask correct password
+userSchema.methods.isPasswordCorrect = async function (password) { 
+    return await bcrypt.compare(password,this.password)
+    // bcrypt.compare(new password in string, current encrypted password) // It will return true or false.
+}
+
+
+// Function to generate access token
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign( // '.sign' method generates the token
+        {
+            _id:this._id, // '_id' is already saved in the db and it does have everyone's access. thats why we need to generate one.
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// Function to generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign( // '.sign' method generates the token
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2' // Allows to write aggregation queries 
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // cloudinary url
+        required:true
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required:true
+    },
+    title: {
+        type: String,
+        required:true
+    },
+    description: {
+        type: String,
+        required:true
+    },
+    duration: {
+        type: String, // cloudinary url
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+
+videoSchema.plugin(mongooseAggregatePaginate) // 'plugin' is a middleware that mongoose allows
+
+
+export const Video = mongoose.model("Video",videoSchema)
