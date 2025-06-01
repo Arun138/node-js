04a8c25088ececd892501e1f06003f4165c5e5d8
Arun <hiarunpradhan@gmail.com>
2025-06-01 14:08:26 +0530

    user route update

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,10 +34,21 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 // secured routes
-router.route('/logout').post(verifyJWT,logoutUser) // here 'verifyJWT' is a middleware. We can use multiple of middlewares in a single route. like '...(verifyJWT,MidWare2,MidWare3,MidWare4,...,logoutUser)'. Just we have to use 'next()' in every middlewares in order to send the request to next middleware or function. So these will execute sequentially.
-router.route('/refresh-token').post(refreshAccessToken) 
+router.route("/logout").post(verifyJWT, logoutUser); // here 'verifyJWT' is a middleware. We can use multiple of middlewares in a single route. like '...(verifyJWT,MidWare2,MidWare3,MidWare4,...,logoutUser)'. Just we have to use 'next()' in every middlewares in order to send the request to next middleware or function. So these will execute sequentially.
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar); // ''upload.single' - its bcz we are checking only single file
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
